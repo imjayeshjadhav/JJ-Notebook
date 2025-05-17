78977182a18128de2ce16b2ebd8e14021c3cabf4
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-05-17 14:51:27 +0530

    Atlas connection and models

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express"
 import Notesrouter from "./routes/notesRoute.js"
+import { connectDB } from "./config/db.js"
+import dotenv from "dotenv"
 
 const app = express()
+const PORT = process.env.PORT || 5001
+
+dotenv.config()
+connectDB()
 
 app.use('/api/notes', Notesrouter)
 
-app.listen(5001,()=>{
-    console.log("Server started on port 5001")
+app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT} `)
 })
 
