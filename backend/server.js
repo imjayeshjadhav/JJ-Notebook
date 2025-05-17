1267d9675054a6805de3cbf0a5b4e16a8e69455e
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-05-18 00:11:55 +0530

    Necessary Changes for Project Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from 'cors'
+import path from 'path'
 import rateLimiter from "./middleware/rateLimitter.js"
 import notesrouter from "./routes/notesRoute.js"
 import { connectDB } from "./config/db.js"
@@ -9,16 +10,27 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV !== "production" ){
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }))
+}
 
 app.use(express.json())
-
-app.use(cors({
-    origin:"http://localhost:5173",
-}))
 
 app.use(rateLimiter)
 
 app.use('/api/notes', notesrouter)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
